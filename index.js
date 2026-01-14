8dd137f1511f0cfa4293ec1653c881ed23b682dc
Allen <129421983+ALLEN-MAKWAKWA@users.noreply.github.com>
2026-01-14 15:18:47 +0200

    HTML FORMS

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 let modal = document.getElementById('modal')
+const closeBtn = document.getElementById('modal-close-btn')
 setTimeout(function(){
-    modal.style.display = 'block'
+    modal.style.display = 'inline'
 },1500)
+
+closeBtn.addEventListener("click",function(){
+     modal.style.display = 'none'
+})
